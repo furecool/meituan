93931e67315e4a60baee661f79c7cb6955917e07
furecool <63301319+furecool@users.noreply.github.com>
2021-04-15 22:33:34 +0800

    Add files via upload

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,10 +1,3 @@
-/* 注意！！！
- * 
- * 早期的vue-cli下面有dev-server.js和dev-client.js两文件，请求本地数据在dev-server.js里配置中，
- * 而因为webpack更新，所以在最新的vue-webpack-template 中已经去掉了dev-server.js和dev-client.js 改用webpack.dev.conf.js代替，
- * 所以 配置本地访问在webpack.dev.conf.js里配置即可。
- * */
-
 'use strict'
 const utils = require('./utils')
 const webpack = require('webpack')
@@ -13,109 +6,43 @@ const merge = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.conf')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
-const portfinder = require('portfinder')
 
-// 导入express
-const express = require('express')
-// 创建express实例
-const app = express()
-// 1、读取json数据
-var goods = require('../data/01-商品页(点菜).json');
-var ratings = require('../data/02-商品页(评价).json');
-var seller = require('../data/03-商品页(商家).json');
+// add hot-reload related code to entry chunks
+// 热加载
+Object.keys(baseWebpackConfig.entry).forEach(function (name) {
+  baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
+})
 
-// 2、路由
-//var routes = express.Router();
-
-// 4、中间件
-//app.use('/api',routes);
-
-
-
-const HOST = process.env.HOST
-const PORT = process.env.PORT && Number(process.env.PORT)
-
-const devWebpackConfig = merge(baseWebpackConfig, {
+// baseWebpackConfig和当前配置进行合并
+module.exports = merge(baseWebpackConfig, {
   module: {
-    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap, usePostCSS: true })
+  	// 传入的参数false
+    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
   },
   // cheap-module-eval-source-map is faster for development
-  devtool: config.dev.devtool,
-
-  // these devServer options should be customized in /config/index.js
-  devServer: {
-    clientLogLevel: 'warning',
-    historyApiFallback: true,
-    hot: true,
-    compress: true,
-    host: HOST || config.dev.host,
-    port: PORT || config.dev.port,
-    open: config.dev.autoOpenBrowser,
-    overlay: config.dev.errorOverlay
-      ? { warnings: false, errors: true }
-      : false,
-    publicPath: config.dev.assetsPublicPath,
-    proxy: config.dev.proxyTable,
-    quiet: true, // necessary for FriendlyErrorsPlugin
-    watchOptions: {
-      poll: config.dev.poll,
-    },
-    // 3、编写接口
-    before(app){
-		app.get('/api/goods', (req,res) => {
-			// 返回数据给客户端，返回json数据
-			res.json(goods);
-		}),
-		app.get('/api/ratings', (req,res) => {
-			// 返回数据给客户端，返回json数据
-			res.json(ratings);
-		}),
-		app.get('/api/seller', (req,res) => {
-			// 返回数据给客户端，返回json数据
-			res.json(seller);
-		})
-	}
-  },
+  devtool: '#cheap-module-eval-source-map',
+  
+  // 插件
   plugins: [
+  	
     new webpack.DefinePlugin({
-      'process.env': require('../config/dev.env')
+      'process.env': config.dev.env
     }),
+    // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
     new webpack.NoEmitOnErrorsPlugin(),
     // https://github.com/ampedandwired/html-webpack-plugin
+    
     new HtmlWebpackPlugin({
+    	  // 编译生成的html文件名
       filename: 'index.html',
+      // 
       template: 'index.html',
+      // 表示打包输出过程中，会自动将.css和.js添加到上述文件中
+      // css默认路径是head标签中
+      // js默认路径是body里面
       inject: true
     }),
+    new FriendlyErrorsPlugin()
   ]
 })
-
-module.exports = new Promise((resolve, reject) => {
-  portfinder.basePort = process.env.PORT || config.dev.port
-  portfinder.getPort((err, port) => {
-    if (err) {
-      reject(err)
-    } else {
-      // publish the new Port, necessary for e2e tests
-      process.env.PORT = port
-      // add port to devServer config
-      devWebpackConfig.devServer.port = port
-
-      // Add FriendlyErrorsPlugin
-      devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
-        compilationSuccessInfo: {
-          messages: [`Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`],
-        },
-        onErrors: config.dev.notifyOnErrors
-        ? utils.createNotifierCallback()
-        : undefined
-      }))
-
-      resolve(devWebpackConfig)
-    }
-  })
-})
-
-
